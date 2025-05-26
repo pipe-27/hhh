5417b426a9c58837ba736337c9916b442cec32dc
pipe-27 <feliperubianoestupinan@gmail.com>
2025-05-26 16:57:41 -0500

    CON

--- connection.py
+++ connection.py
@@ -3,8 +3,10 @@ from pymongo.server_api import ServerApi
 
 
 def connect_to_mongodb(db_name, collection_name):
-    uri = "mongodb+srv://Solangie:Sesc120512*@cluster0.2fpa3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    uri = "mongodb+srv://frubiano811:IFMER@cluster0.uqlg42r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client[db_name]
     collection = db[collection_name]
     return collection
+
+    
